54620ca3f68b6cee1104fd053ecd7b0cffe175da
Ahmed Hawam <ahmed_hawam1998@outlook.com>
2023-02-06 19:22:39 +0200

    mastery

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
+//=========== Active navbar Link in multi pages =============
 const currentLocation = location.href;
-const menuItem = document.querySelectorAll(".navbar-collapse a");
+const menuItem = document.querySelectorAll(
+  ".navbar .nav-item .nav-link"
+);
 const menuLength = menuItem.length;
 for (let i = 0; i < menuLength; i++) {
   if (menuItem[i].href === currentLocation) {
-    menuItem[i].className = "active";
+    menuItem[i].className = "nav-link active";
   }
 }
+//=========== Active navbar Link in multi pages =============
